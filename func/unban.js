9fa85f09d6c4e5b13ed62815df4e81127c70019a
ViziPL <82292468+tobiasgranie@users.noreply.github.com>
2022-02-05 16:17:32 +0100

    Update unban.js

--- func/unban.js
+++ func/unban.js
@@ -17,14 +17,14 @@ export async function handler(event, context) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
+                        "Location": `/success?msg=${encodeURIComponent("Użytkownik został odbanowany\nSkontaktuj się z nim, i powiedz mu o tym.")}`
                     }
                 };
             } catch (e) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/error?msg=${encodeURIComponent("Failed to unban user\nPlease manually unban")}`
+                        "Location": `/error?msg=${encodeURIComponent("Wystąpił problem!\nOdbanuj go manualnie")}`
                     }
                 };
             }
